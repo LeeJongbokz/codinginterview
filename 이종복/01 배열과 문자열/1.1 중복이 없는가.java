803aaef482a65bd25dad90942ee974c1e9520344
LeeJongbok <s2feeling@naver.com>
2022-04-13 13:36:05 +0900

    Update 1.1 중복이 없는가.java

--- 이종복/01 배열과 문자열/1.1 중복이 없는가.java
+++ 이종복/01 배열과 문자열/1.1 중복이 없는가.java
@@ -1,1 +1,24 @@
-dd
+import java.util.*;
+public class Main {	
+	public boolean solution(String s){
+	
+	   char[] stringToChar = s.toCharArray();
+	   Arrays.sort(stringToChar);
+	   String sortedStr = new String(stringToChar);
+	   
+	   for(int i=0; i<sortedStr.length()-1; i++){
+	       if(sortedStr.charAt(i) == sortedStr.charAt(i+1)){
+	           return true;
+	       }
+	   }
+	     
+	   return false;
+	}
+
+	public static void main(String[] args){
+		Main T = new Main();
+		Scanner kb = new Scanner(System.in);
+		String str=kb.next();
+		System.out.println(T.solution(str));
+	}
+}
